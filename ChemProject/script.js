a85b5bc680aef4d646e48712dc861d7508081d67
dprozz <dmitri.proskuriakov@gmail.com>
2019-11-29 12:03:31 +0300

    new changes

--- ChemProject/script.js
+++ ChemProject/script.js
@@ -1,4 +1,8 @@
-
+var request = new XMLHttpRequest();
+request.open("experements.json");
+request.send(null);
+jsonexperiments = JSON.parse("request.responseText",false);
+console.log(jsonexperiments);
 
 function pasteExperement(){
     leftmenuelem = document.getElementById("block-menu").getElementsByClassName("ul");
