6479e2deb79644fbe454b40b27bcc5e3aca48ad1
dmitri <dmitri.proskuriakov@gmail.com>
2019-12-12 18:20:11 +0300

    transefr data

--- experimentsBoxAlpha/static/script.js
+++ experimentsBoxAlpha/static/script.js
@@ -1,12 +1,19 @@
 leftmenuelem = document.getElementById("inmenu");
 maininfoelem = document.getElementById("right");
 
-function createPageForExperiments(data){
+"<img src={{url_for('static',filename='pharaoh1.jpg}}')}>"
+
+function init(contex,contab){
+    window.experimentsInfo = contex;
+    window.aboutInfo = contab;
+};
+
+function createPageForExperiments(){
     leftmenuelem.innerHTML = " ";
     maininfoelem.innerHTML = "<h2>Нажмите на опыт слева чтобы просмотреть его</h2>";
-    for(var key in data){
+    for(var key in window.experimentsInfo){
         var l = document.createElement("li");
-        l.innerHTML = data[key]["title"];
+        l.innerHTML = window.experimentsInfo[key]["title"];
         oncl = "pasteExperement('" + key + "')";
         l.setAttribute("onclick",oncl);
         l.setAttribute("id","clickable");
@@ -18,9 +25,9 @@ function createPageForExperiments(data){
 function createPageAbout(){
     leftmenuelem.innerHTML = " ";
     maininfoelem.innerHTML = "<h2>Нажмите на вкладку слева чтобы просмотреть информацию</h2>";
-    for(var key in aboutInfo){
-        var l = document.createElement("li");
-        l.innerHTML = aboutInfo[key]["title"];
+    for(var key in window.aboutInfo){
+        var l = window.document.createElement("li");
+        l.innerHTML = window.aboutInfo[key]["title"];
         oncl = "pasteInfo('" + key + "')";
         l.setAttribute("onclick",oncl);
         l.setAttribute("id","clickable");
@@ -31,16 +38,21 @@ function createPageAbout(){
 
 function pasteExperement(elem){
     var h = document.createElement("h1");
-    h.innerHTML = experimentsInfo[elem]["title"];
+    h.innerHTML = window.experimentsInfo[elem]["title"];
     maininfoelem.innerHTML = " ";
     maininfoelem.appendChild(h);
-    maininfoelem.innerHTML += experimentsInfo[elem]["text"];
+    maininfoelem.innerHTML += window.experimentsInfo[elem]["text"];
+    if (window.experimentsInfo[elem]["imgatr"] != null){
+        for(var key = 0; key < window.experimentsInfo[elem]["imgatr"].length; key++){
+            maininfoelem.innerHTML += "<img src={{url_for('static', filename='${window.experimentsInfo[elem]['imgatr'][key]}')}}>"
+        }
+    };
 };
 
 function pasteInfo(page){
     var h = document.createElement("h1");
-    h.innerHTML = aboutInfo[page]["title"];
+    h.innerHTML = window.aboutInfo[page]["title"];
     maininfoelem.innerHTML = " ";
     maininfoelem.appendChild(h);
-    maininfoelem.innerHTML += aboutInfo[page]["text"];
+    maininfoelem.innerHTML += window.aboutInfo[page]["text"];
 }
