f8f5bc1b26093b63bfa73a7c43e7b87c4c71d080
dprozz <dmitri.proskuriakov@gmail.com>
2019-12-17 19:57:01 +0300

    Dima dibil

--- experimentsBoxAlpha/static/script.js
+++ experimentsBoxAlpha/static/script.js
@@ -3,10 +3,17 @@ maininfoelem = document.getElementById("main");
 
 function init(){
     var xhr = new XMLHttpRequest();
+    xhr.open("GET","/getAboutInfo",false);
+    xhr.send(null);
+    if (xhr.status == 200) window.aboutInfo = JSON.parse(xhr.responseText);
+
+    var xhr = new XMLHttpRequest();
     xhr.open("GET","/getExperimentsInfo",false);
     xhr.send(null);
     if (xhr.status == 200) window.experimentsInfo = JSON.parse(xhr.responseText);
+}
 
+function createPageForExperiments(){
     leftmenuelem.innerHTML = " ";
     maininfoelem.innerHTML = "<h2>Нажмите на опыт слева чтобы просмотреть его</h2>";
     for(var key in window.experimentsInfo){
@@ -17,9 +24,30 @@ function init(){
         l.setAttribute("id","clickable");
         leftmenuelem.appendChild(l);
     };
+};
+
+function createPageForInfo(){
+    leftmenuelem.innerHTML = " ";
+    maininfoelem.innerHTML = "<h2>Нажмите на вкладку слева чтобы просмотреть информацию</h2>";
+    for(var key in window.aboutInfo){
+        var l = window.document.createElement("li");
+        l.innerHTML = window.aboutInfo[key]["title"];
+        oncl = "pasteInfo('" + key + "')";
+        l.setAttribute("onclick",oncl);
+        l.setAttribute("id","clickable");
+        leftmenuelem.appendChild(l);
+    };
 }
 
-function pasteExperement(elem){
+function pasteInfo(page){
+    var h = document.createElement("h1");
+    h.innerHTML = window.aboutInfo[page]["title"];
+    maininfoelem.innerHTML = " ";
+    maininfoelem.appendChild(h);
+    maininfoelem.innerHTML += window.aboutInfo[page]["text"];
+}
+
+function pasteExperiment(elem){
     var h = document.createElement("h1");
     h.innerHTML = window.experimentsInfo[elem]["title"];
     maininfoelem.innerHTML = " ";
